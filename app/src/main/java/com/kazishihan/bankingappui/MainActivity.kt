c3639fcbdac54d6e629f593efacb8e071c2b5993
Kazi Ariful Islam <kazi.ariful@nexdecade.com>
2024-05-28 12:36:39 +0600

    theme switching implementation

--- app/src/main/java/com/kazishihan/bankingappui/MainActivity.kt
+++ app/src/main/java/com/kazishihan/bankingappui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +14,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,9 +30,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BankingAppUITheme {
+            var isDarkTheme by remember { mutableStateOf(true) }
+            BankingAppUITheme(darkTheme = isDarkTheme) {
                 setBarColor(color = MaterialTheme.colorScheme.background)
-                HomeScreen()
+                HomeScreen(){
+                    isDarkTheme = it
+                }
 //                Scaffold(
 //                    modifier = Modifier
 //                        .fillMaxSize()
@@ -62,9 +68,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview
+
 @Composable
-fun HomeScreen() {
+fun HomeScreen(switchTheme: (Boolean) -> Unit) {
     Scaffold(bottomBar = {
         BottomNavigationBar()
     }) { padding ->
@@ -74,11 +80,13 @@ fun HomeScreen() {
                 .padding(padding)
         ) {
             
-            WalletSection()
+            WalletSection(){
+                switchTheme.invoke(it)
+            }
             CardsSection()
             Spacer(modifier = Modifier.height(16.dp))
             FinanceSection()
-            // CurrenicesSection()
+            CurrenciesSection()
         }
     }
 }
